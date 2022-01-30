16fc7e4e8cdd0018ea3a47bb527bc7423a6cf9b6
miguel <mikemartinezmestre@gmail.com>
2022-01-30 01:55:40 +0100

    actualizado

---
+++ components/Header.js
@@ -0,0 +1,45 @@
+import { useQuery, gql } from "@apollo/client";
+import { useRouter } from "next/router";
+
+const usuario = gql`
+query ObtenerUsuario {
+  obtenerUsuario {
+    nombre
+  }
+}
+`;
+
+
+
+const Header = () => {
+
+    const {data,loading,error} = useQuery(usuario);
+    const router = useRouter();
+
+    function cerrarSesion(){
+        localStorage.removeItem("token");
+        router.push("/login");
+    }
+
+
+    if(loading) return "Cargando...";
+
+    if(!data){
+        return router.push("/login");
+    }
+
+    console.log(data.obtenerUsuario)
+    return (  
+        <div className="flex justify-end">
+
+              <p className="mr-2">Hola {data.obtenerUsuario.nombre}</p>                                              
+
+             <button type="button" className="bg-blue-800 w-full sm:w-auto font-bold uppercase text-xs rounded py-1 px-2 text-white shadow-md" onClick={()=>{cerrarSesion()}}>
+                Cerrar Sesion
+             </button>
+
+        </div>
+     );
+}
+ 
+export default Header;
